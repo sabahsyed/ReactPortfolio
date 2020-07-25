f8671b1943b7295933331a2f0d5dc3d2803b60d1
Sabah Syed <sabisyed@gmail.com>
2020-07-24 19:18:35 -0700

    Added links page, no styles added

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -4,6 +4,15 @@ export class Links extends Component {
     render() {
         return (
             <div>
+                <div class = "row ">
+                    <div class = "col-md-12">
+                        <div class="card-body">
+                            <h5>Github Profile :<a href="https://github.com/sabahsyed">SabahSyedGitHub</a></h5>
+                            <h5>LinkedIn Profile :<a href="https://www.linkedin.com/in/sabah-syed-89a07344/">SabahSyedLinkedIn</a></h5>
+                            <h5> Contact me :<a href="mailto:sabisyed@gmail.com">Click Here To Email Me</a></h5>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         )
