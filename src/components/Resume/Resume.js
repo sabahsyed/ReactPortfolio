dd090abc97d6b821c9047ef1ceb9dd7c0b8e9039
Sabah Syed <sabisyed@gmail.com>
2020-07-24 18:47:43 -0700

    Resume added as a page, no styles added

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import "./Resume.css"
+import resume from "../Assets/resume/SabahSyedResume.pdf"
 export class Resume extends Component {
     render() {
         return (
             <div>
-                
+               <a class="nav-link active animate__animated animate__backInRight" href={resume}>Resume</a> 
             </div>
         )
     }
