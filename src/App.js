eac694a89cb5a98ec05128777d2899736ab6a1c0
Sabah Syed <sabisyed@gmail.com>
2020-07-24 18:40:51 -0700

    Added home page and styles to it

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter as Router,Route
+} from "react-router-dom";
+
+import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import Resume from "./components/Resume/Resume";
+import Projects from "./components/Projects/Projects";
+import Links from "./components/Links/Links";
+import Footer from "./components/Footer/Footer"
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Resume" component={Resume} />
+        <Route exact path="/Projects" component={Projects} />
+        <Route path="/Links" component={Links} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ test/src/components/Home/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import "./Home.css"
+import myImage from "../Assets/images/sabahSyed.jpeg"
+//import BackgroundImage from "../Background/backgroundImage.jpg"
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+            {/* <BackgroundImage /> */}
+                    <div className = "row">
+                        <div className = "col-md-6 aboutMe">
+                            <h5 className="card-title animate__animated animate__lightSpeedInLeft">A little about me...</h5>
+                                <p className="card-text animate__animated animate__lightSpeedInLeft"/>I am a stay-at-home mom currently enrolled in a bootcamp at the University of Washington.
+                                I do not have any professional experience but through this journey I have grown an immense 
+                                passion for coding and would like to turn this into a career I always wanted to have. 
+                                Feel free to look at my most recent work. 
+                                <p />
+                        <div className = "col-md-6">
+                            <img className="myImage" src={myImage} alt="SabahSyed"  />
+                        </div>
+                        </div>
+                    </div>
+            </div>
+        )
+    }
+}
+
+export default Home
